bda6ed27fe0275ad28be9f8d0c22984a225627c2
mateusz0190 <mateusz0190@gmail.com>
2023-01-28 13:43:57 +0100

    Initial commit

---
+++ src/test/java/com/example/personalfinances/PersonalFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personalfinances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinancesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
